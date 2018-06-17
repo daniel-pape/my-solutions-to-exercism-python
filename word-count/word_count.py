30d73da49e1a26c72d7a6fb14299f930edb305cd
dp <daniel.pape@codecentric.de>
2018-06-17 21:21:31 +0200

    Clean-up

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -11,22 +11,12 @@ def word_count(phrase: str):
         return tokens
 
     def count_tokens(tokens: list):
-        keys = set(tokens)
-
-        counts = dict([(key, 0) for key in keys])
+        counts = dict()
 
         for t in tokens:
-            counts[t] += 1
+            counts[t] = counts.get(t, 0) + 1
 
         return counts
-
-    # def count_tokens_alt(tokens: list):
-    #     counts = dict()
-    #
-    #     for t in tokens:
-    #         counts[t] = counts.get(t, 0) + 1
-    #
-    #     return counts
 
     tokens = tokenize(phrase)
     result = count_tokens(tokens)
